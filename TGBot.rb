6f3d738279244480cfe6511984fad8e2d726b729
Mambavoz <SirDknv@gmail.com>
2026-03-23 16:10:04 +0300

    Add some functions for checking video

--- TGBot.rb
+++ TGBot.rb
@@ -11,26 +11,35 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
     case message
     when Telegram::Bot::Types::Message
+      if (valid_youtube_link?(message) && youtube_video_exists?(message, "AIzaSyBbB4Rs7TQTSzVDQXNgKr0AVnXuBoaA6iA")) | valid_rutube_link?(message)
         redis.set("user_url_#{message.chat.id}", message.text)
         redis.expire("user_url_#{message.chat.id}", 3600)
 
-        kb = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: [
-          [
-            Telegram::Bot::Types::InlineKeyboardButton.new(text: "Video", callback_data: "form_vid"),
-            Telegram::Bot::Types::InlineKeyboardButton.new(text: "Audio", callback_data: "form_aud")
-          ]
-        ])
-        bot.api.send_message(chat_id: message.chat.id, text: 'Select the format:', reply_markup: kb)
-
+        bot.api.send_message(chat_id: message.chat.id, text: 'Select the format:', reply_markup: initial_keyboard)
+      end
     when Telegram::Bot::Types::CallbackQuery
-      url = redis.get("user_url_#{message.message.chat.id}")
-      if url
-        bot.api.send_message(chat_id: message.message.chat.id, text: "Задача добавлена в очередь! Начинаю обработку...")
+      chat_id = message.message.chat.id
+      url = redis.get("user_url_#{chat_id}")
 
-        format_type = message.data == "form_vid" ? "video" : "audio"
-        VideoWorker.perform_async(message.message.chat.id, url, format_type)
-      else
-        bot.api.send_message(chat_id: message.message.chat.id, text: "Ссылка устарела или потеряна. Отправьте видео заново.")
+      case message.data
+      when "form_vid"
+        bot.api.send_message(chat_id: chat_id, text: "Выберите разрешение:", reply_markup: video_quality_keyboard)
+
+      when "form_aud"
+        bot.api.send_message(chat_id: chat_id, text: "Выберите формат аудио:", reply_markup: audio_format_keyboard)
+
+      when "form_prev"
+        VideoWorker.perform_async(chat_id, url, "preview")
+
+      when /^vid_(\d+)$/  # например vid_720
+        quality = $1
+        bot.api.send_message(chat_id: chat_id, text: "Задача добавлена! Обрабатываю видео #{quality}p...")
+        VideoWorker.perform_async(chat_id, url, "video", quality)
+
+      when /^aud_(\w+)$/  # например aud_mp3
+        format = $1
+        bot.api.send_message(chat_id: chat_id, text: "Задача добавлена! Обрабатываю аудио (#{format})...")
+        VideoWorker.perform_async(chat_id, url, "audio", format)
       end
     end
   end
